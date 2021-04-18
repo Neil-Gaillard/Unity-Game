f6737786ae950ca7a0db1c17649b10a3c198c163
Neil <neil.gaillard@hotmail.com>
2021-04-18 16:15:56 +0200

    Added CameraCheck.cs

---
+++ Assets/Scripts/Camera/CameraCheck.cs
@@ -0,0 +1,13 @@
+using System;
+using UnityEngine;
+
+namespace Camera
+{
+    public class CameraCheck : MonoBehaviour
+    {
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
