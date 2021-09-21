96984be3347421745975d9b256eed87c0ff06946
neilGaillard <neil.gaillardpro@gmail.com>
2021-09-21 21:10:53 +0200

    Updated ReWritePlayerController.cs

--- Assets/Scripts/Player/ReWritePlayerController.cs
+++ Assets/Scripts/Player/ReWritePlayerController.cs
@@ -1,4 +1,4 @@
-using System;
+using System.Collections;
 using UnityEngine;
 
 namespace Player
@@ -88,26 +88,107 @@ namespace Player
         
         private void FixedUpdate()
         {
-            throw new NotImplementedException();
+            //Checks if Player is on ground
+            _isOnGround = _groundCheck.isOnGround();
+
+            //Counts how many times the player leaves the ground, and if they jump after 
+            _jumpCount = _isOnGround switch
+            {
+                true => 0,
+                false when _jumpCount == 0 => 1,
+                _ => _jumpCount
+            };
+
+            if (_isOnGround)
+                _isJumping = false;
+            
+            //PRESSION JUMPING
+            if (_isJumping)
+                if (!_jumpKeyHeld && Vector2.Dot(_playerRigidbody2D.velocity, Vector2.up) > 0)
+                    _playerRigidbody2D.AddForce(new Vector2(0, -150) * DefaultMass);
+            
+            if (Input.GetKey(KeyCode.A) && _canDash)
+                Dash();
+            
+            _horizontalInput = Input.GetAxis("Horizontal");
+
+            if (_horizontalInput < 0)
+            {
+                _orientation = Orientation.Left;
+                _playerSpriteRenderer.flipX = true;
+            }
+            else if (_horizontalInput > 0)
+            {
+                _orientation = Orientation.Right;
+                _playerSpriteRenderer.flipX = false;
+            }
+
+            Move();
         }
 
         private void Update()
         {
-            throw new NotImplementedException();
+            // --- JUMPING ---
+            
+            //If the player is releasing jump button, we put the value to true
+            //This test will prevent continuous Jumping
+            if (!_canJump && Input.GetKeyUp(KeyCode.Space))
+                _canJump = true;
+
+            //If the player presses the jump button, it is indicated to _jumpKeyHeld and he jumps if he is on the ground
+            //This lauched the Initial Jump from the ground
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                _jumpKeyHeld = true;
+                if (_isOnGround)
+                    Jump();
+            }
+
+            //Checks for jump button release
+            if (Input.GetKeyUp(KeyCode.Space))
+                _jumpKeyHeld = false;
+            
+            // --- ATTACKING ---
+
+            if (_canLaunchProjectiles && Input.GetKey(KeyCode.Z))
+            {
+                _attackSpawnManager.SpawnProjectile();
+                StartCoroutine(AttackDelay());
+            }
+            
+            // --- RESPAWN ---
+            if (transform.position.y < -40)
+            {
+                transform.localPosition = _defaultPosition;
+                SetPlayerVelocity(0, 0);
+            }
         }
         
         // ------ COLLISIONS DETECTION METHODS ------
 
-        private void OnCollisionEnter2D(Collision2D other)
-        {
-            throw new NotImplementedException();
-        }
-
         private void OnTriggerEnter2D(Collider2D other)
         {
-            throw new NotImplementedException();
+            if (other.gameObject.CompareTag("Token"))
+                Destroy(other.gameObject);
+            if (other.gameObject.CompareTag("PointDouble"))
+            {
+                _canDoubleJump = true;
+                Destroy(other.gameObject);
+            }
+
+            if (other.gameObject.CompareTag("PointProjectile"))
+            {
+                _canLaunchProjectiles = true;
+                Destroy(other.gameObject);
+            }
+
+            if (other.gameObject.CompareTag("PointDash"))
+            {
+                _canDash = true;
+                Destroy(other.gameObject);
+            }
         }
-        
+
         // ------ GETTERS ------
 
         // ------ SETTERS ------
@@ -116,30 +197,50 @@ namespace Player
         {
             _playerRigidbody2D.velocity = new Vector2(x, y);
         }
+
+        private void SetProjectileSpeed(float speed)
+        {
+            _projectileSpeed = speed;
+        }
         
         // ------ PLAYER ACTIONS ------
 
         private void Move()
         {
-            
+            SetPlayerVelocity(_speed * _horizontalInput, _playerRigidbody2D.velocity.y);
         }
 
-        private void CalculateJumpForce()
+        private float CalculateJumpForce()
         {
-            
+            return Mathf.Sqrt(2 * Physics2D.gravity.magnitude * DefaultJumpForce);
         }
 
         private void Jump()
         {
-            
+            SetPlayerVelocity(_playerRigidbody2D.velocity.x, 0);
+            _isJumping = true;
+            _playerRigidbody2D.AddForce(Vector2.up * (CalculateJumpForce() * _playerRigidbody2D.mass),
+                ForceMode2D.Impulse);
+            _isOnGround = false;
+            _canJump = false;
+            ++_jumpCount;
         }
 
         private void Dash()
         {
-            
+            var velocity = _playerRigidbody2D.velocity;
+            Vector3 movdir = new Vector2(velocity.x, velocity.y).normalized;
+            transform.position += movdir * _dashSpeed;
         }
         
         // ------ COROUTINES ------
+
+        private IEnumerator AttackDelay()
+        {
+            _canLaunchProjectiles = false;
+            yield return new WaitForSeconds(_projectileDelay);
+            _canLaunchProjectiles = true;
+        }
 
         //TODO: Encapsuler l'Orientation
         private enum Orientation
