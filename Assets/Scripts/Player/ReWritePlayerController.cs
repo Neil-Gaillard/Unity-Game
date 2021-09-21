98b6c53a9ed6a60c4bfeb85a51c77c7c7aad563b
neilGaillard <neil.gaillardpro@gmail.com>
2021-09-21 12:59:31 +0200

    Modified ReWritePlayerController.cs

--- Assets/Scripts/Player/ReWritePlayerController.cs
+++ Assets/Scripts/Player/ReWritePlayerController.cs
@@ -1,19 +1,96 @@
+using System;
 using UnityEngine;
 
 namespace Player
 {
     public class ReWritePlayerController : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
-        {
+        // ------ CONSTANTS ------
+
+        // --- Default Physics Values ---
+        private const float DefaultMass = 60.0f;
+        private const float GravityModifier = 3.0f; //fall speed
+        private const float GravityScale = 2.5f; //gravity influence
+
+        // --- Default Position Values ---
+        private const float DefaultPositionX = 0.0f;
+        private const float DefaultPositionY = 0.0f;
+        private const float DefaultPositionZ = 0.0f;
+
+        // --- Default Characteristics ---
+        private const float DefaultSpeed = 15.0f;
+
+        private const float DefaultJumpForce = 20.0f;
+
+        private const float DefaultDashSpeed = 5.0f;
+
+        private const float DefaultProjectileSpeed = 30.0f;
+        private const float DefaultProjectileDelay = 0.5f;
         
+        // ------ PRIVATE ATTRIBUTES ------
+
+        private readonly Vector3 _defaultPosition = new Vector3(DefaultPositionX, DefaultPositionY, DefaultPositionZ);
+
+        // --- Player abilities ---
+        private bool _canJump; //(can press the button to jump)
+        private bool _canLaunchProjectiles;
+        private bool _canDash;
+        private bool _canDoubleJump;
+        
+        // --- Player state ---
+        private bool _isJumping;
+        private bool _isOnGround;
+        
+        // --- Key Pressing Check ---
+        private bool _jumpKeyHeld;
+
+        private float _horizontalInput;
+
+        // --- Initialisation of values from constants ---
+        private float _dashSpeed = DefaultDashSpeed;
+        private float _projectileDelay = DefaultProjectileDelay;
+        private float _projectileSpeed = DefaultProjectileSpeed;
+        private readonly float _speed = DefaultSpeed;
+
+        [Range(0, 2)] private int _jumpCount;
+        
+        private Orientation _orientation = Orientation.Left;
+
+        // ------ PRIVATE REFERENCES ------
+        
+        // --- Own References ---
+        private Rigidbody2D _playerRigidbody2D;
+        private SpriteRenderer _playerSpriteRenderer;
+        
+        // --- Other References ---
+        private AttackSpawnManager _attackSpawnManager;
+        private GroundCheck _groundCheck;
+
+        // ------ EVENT METHODS ------
+
+        private void Awake()
+        {
+            //Initializing References
+            _playerRigidbody2D = GetComponent<Rigidbody2D>();
+            _playerSpriteRenderer = GetComponent<SpriteRenderer>();
+            
+            _attackSpawnManager = GameObject.Find("LaserSpawnManager").GetComponent<AttackSpawnManager>();
+            _groundCheck = GameObject.Find("Ground Check").GetComponent<GroundCheck>();
         }
 
-        // Update is called once per frame
-        void Update()
+        private void Start()
         {
-        
+            //Initializing Physics Properties
+            Physics2D.gravity *= GravityModifier;
+            _playerRigidbody2D.gravityScale = GravityScale;
+            _playerRigidbody2D.mass = DefaultMass;
+        }
+
+        //TODO: Encapsuler l'Orientation
+        private enum Orientation
+        {
+            Left = -1,
+            Right = 1
         }
     }
 }
